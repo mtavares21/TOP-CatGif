7d752fd090e43ba1b5010bee9ad92c246618f651
mtavares21 <migtavares6@gmail.com>
2021-05-05 18:39:58 +0100

    now with search bar

--- gif.js
+++ gif.js
@@ -8,8 +8,26 @@ const getCat = () => {
       img.src = response.data.images.original.url;
     })
     .catch((error) => {
-      console.log(error);
+      img.src = 'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=ZhplzyJ9';
       alert('Ups, cannot fetch the cat rigth about now! Please try again later.');
     });
 };
 getCat();
+
+const searchGif = () => {
+  const img = document.querySelector('img');
+  const search = document.getElementById('search');
+  const userInput = search.value;
+  fetch(`https://api.giphy.com/v1/gifs/search?api_key=leBI4z9YLQleG5lKlrmz96foCSXKCGRD&q=${userInput}&limit=25&offset=0&rating=g&lang=en`, {
+    mode: 'cors',
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      let index = Math.floor(Math.random()*Array.from(response.data).length)
+      img.src = response.data[index].images.original.url;
+    })
+    .catch((error) => {
+      img.src = 'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=ZhplzyJ9';
+      alert('Ups, cannot fetch the cat rigth about now! Please try again later.');
+    });
+};
